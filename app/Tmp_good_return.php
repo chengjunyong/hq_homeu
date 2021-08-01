7fcba5b52153f10ee1b3c2bdd39c7982b9a5a293
chengjunyong <junyong1213@hotmail.com>
2021-08-01 12:28:52 +0800

    Fix model

--- app/Tmp_good_return.php
+++ app/Tmp_good_return.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tmp_good_return extends Model
 {
-    protected $table = 'Tmp_good_return';
+    protected $table = 'tmp_good_return';
     protected $fillable = 
     [
       'barcode',
