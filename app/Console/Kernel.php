62304db04f78fc90306fd5dd84065d7ed61bcee2
chengjunyong <junyong1213@hotmail.com>
2023-03-14 12:20:15 +0800

    Test Command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,7 +26,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('cron:syncStock')->everyMinute();
+        $schedule->command('cron:syncStock')->hourly();
         $schedule->command('test:job')->everyMinute();
     }
 
