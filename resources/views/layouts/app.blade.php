02149ec13588602905dca88dadfdd513e3018adc
chengjunyong <junyong1213@hotmail.com>
2024-02-02 18:41:29 +0800

    update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -67,6 +67,14 @@
   height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
   <!-- End Google Tag Manager (noscript) -->
 
+  <script type="text/javascript">
+      (function(c,l,a,r,i,t,y){
+          c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+          t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+          y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+      })(window, document, "clarity", "script", "kvfrtfauqv");
+  </script>
+
   <button class="float" onclick="window.location.assign('{{ (isset($url)) ? $url : "" }}')" style="border:none">
     <i class="fa fa-arrow-left" style="font-size: 40px;"></i>
   </button>
